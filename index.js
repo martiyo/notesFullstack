c4df95d2dda50cffc55a361ea31d911f16f55a7e
dev1 <martiyosrc@gmail.com>
2021-03-14 17:17:41 -0300

    fix port

--- index.js
+++ index.js
@@ -82,6 +82,6 @@ const unknowEndpoint = (request, response) => {
   response.status(404).send({ error: "unknow endpoint" });
 };
 
-const PORT = proces.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT);
 console.log(`Server running on port ${PORT}`);
