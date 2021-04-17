02d2215accaebf3e0a4d17c5d855eab3726d45f5
dev1 <martiyosrc@gmail.com>
2021-04-17 20:09:19 -0300

    test backend

---
+++ app.js
@@ -0,0 +1,37 @@
+const config = require("./utils/config");
+const express = require("express");
+require("express-async-errors");
+const app = express();
+const cors = require("cors");
+const notesRouter = require("./controllers/notes");
+const middleware = require("./utils/middleware");
+const logger = require("./utils/logger");
+const mongoose = require("mongoose");
+
+logger.info("connecting to", config.MONGODB_URI);
+
+mongoose
+  .connect(config.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then((result) => {
+    console.log("connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log("error connecting to MongoDB:", error.message);
+  });
+
+app.use(express.json());
+app.use(cors());
+app.use(express.static("build"));
+app.use(middleware.requestLogger);
+
+app.use("/api/notes", notesRouter);
+
+app.use(middleware.errorHandler);
+app.use(middleware.errorHandler);
+
+module.exports = app;
