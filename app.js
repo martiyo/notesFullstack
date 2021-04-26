992725bf171c246ed8671f5d7a0a05791ee10dda
dev1 <martiyosrc@gmail.com>
2021-04-26 20:57:54 -0300

    testing backend complete

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ const express = require("express");
 require("express-async-errors");
 const app = express();
 const cors = require("cors");
-const notesRouter = require("./controllers/notes");
+
 const middleware = require("./utils/middleware");
 const logger = require("./utils/logger");
 const mongoose = require("mongoose");
+
+const notesRouter = require("./controllers/notes");
+const usersRouter = require("./controllers/users");
 
 logger.info("connecting to", config.MONGODB_URI);
 
@@ -30,6 +33,7 @@ app.use(express.static("build"));
 app.use(middleware.requestLogger);
 
 app.use("/api/notes", notesRouter);
+app.use("/api/users", usersRouter);
 
 app.use(middleware.errorHandler);
 app.use(middleware.errorHandler);
